1423c356d7792e02a6cf42d7e75a30e4f6cafbbb
denet <deneth@hotmail.co.uk>
2021-10-26 11:56:16 +0100

    Exercise 3 parts 1-3 (matrices) completion

--- exercise_3.py
+++ exercise_3.py
@@ -1,19 +1,48 @@
 import math
 
-
-# def matrixmult(A, B):
-#     for i in range(0, len(A)):
-        # for j in A[i]:
-    # for x in range(0,len(B))
-
 A = [[2, 1], [4, 8], [7, 5]]
 B = [[4, 6], [2, 7]]
-# matrixmult(A, B)
-print(len(B))
-'''
-[2*4+1*2],[2*6+1*7]
-[4*4+8*2],[4*6+8*7]
-[7*4+5*2],[7*6+5*7]
 
-[00*00+01*11],[00*01+01*11]
-'''
+
+def matrixmult(A, B):
+
+    '''
+    :param A: 2D nested list; the matrix that is the multiplier
+    :param B: 2D nested list; the matrix that is the multiplicand
+    :return: 2D nested list; product of the matrix multiplication
+
+    --------------------------------------------------------------
+    function calculates the matrix product of the to matrix arguments
+    and raises errors if appropriate
+
+    '''
+
+    if len(A[0]) != len(B):
+        raise ValueError("Cannot multiply the two matrices A and B. Number of columns in A is not equal to the number of rows in B")
+    for i in A:
+        if len(i) != len(A[0]):
+            raise ValueError("Matrix A is invalid, number of columns in a row is not consistent")
+    for i in B:
+        if len(i) != len(B[0]):
+            raise ValueError("Matrix B is invalid, number of columns in a row is not consistent")
+
+    product_matrix = []
+    for i in range(0, len(A)):
+        row_element = []
+        for j in range(0, len(B[0])):
+            element_sum = 0
+            for k in range(0, len(B)):
+                element_sum += A[i][k] * B[k][j]
+            row_element.append(element_sum)
+        product_matrix.append(row_element)
+    return product_matrix
+
+x = math.pi / 2
+A = [[math.cos(x), -math.sin(x)], [math.sin(x), math.cos(x)]]
+B = [[1], [0, 0]]
+try:
+    C = matrixmult(A, B)
+except ValueError as inst:
+    print("Caught ValueError")
+    print("You have thrown the right type of error")
+
